a4c123846621a1d05450c70598240b2982daa7a3
Panya Farnette <naienko@gmail.com>
2019-07-31 17:30:16 -0500

    added context class

---
+++ SpentBulletsAPI/Models/Context.cs
@@ -0,0 +1,21 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+
+namespace SpentBulletsAPI.Models
+{
+    public class Context
+    {
+        public string ConnectionString { get; set; }
+
+        public Context(string connectionString)
+        {
+            this.ConnectionString = connectionString;
+        }
+
+        private MySqlConnection GetConnection()
+        {
+            return new MySqlConnection(ConnectionString);
+        }
+    }
+}
--- SpentBulletsAPI/Startup.cs
+++ SpentBulletsAPI/Startup.cs
@@ -10,6 +10,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
+using SpentBulletsAPI.Models;
 
 namespace SpentBulletsAPI
 {
@@ -39,6 +40,7 @@ namespace SpentBulletsAPI
                         .AllowAnyMethod();
                 });
             });
+            services.Add(new ServiceDescriptor(typeof(Context), new Context(Configuration.GetConnectionString("DefaultConnection"))));
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
         }
 
